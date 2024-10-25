e9bc45d0a5328057b35bc487888eb537067968ad
Yaroslav Klimenko <klimyarik13@gmail.com>
2024-10-25 12:43:24 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -22,12 +22,14 @@ export const setupServer = () => {
 
   app.get('/', (req, res) => {
     res.json({
+      status: 200,
       message: 'Hello, World!',
     });
   });
   app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts();
     res.status(200).json({
+      status: 200,
       message: 'Successfully found contacts!',
       data: contacts,
     });
@@ -38,10 +40,12 @@ export const setupServer = () => {
     const contact = await getContactById(contactId);
     if (!contact) {
       return res.status(404).json({
+        status: 404,
         message: 'Contact not found',
       });
     }
     res.status(200).json({
+      status: 200,
       message: `Successfully found contact with id ${contactId}!`,
       data: contact,
     });
@@ -49,6 +53,7 @@ export const setupServer = () => {
 
   app.use('*', (req, res, next) => {
     res.status(404).json({
+      status: 404,
       message: 'Not Found',
     });
   });
