200a9d9b4d900683b5e33dfdaa852c1807a014c2
Yaroslav Klimenko <klimyarik13@gmail.com>
2024-10-30 12:14:00 +0200

    312

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 
 import {
+  createContactController,
+  deleteContactController,
   getAllContactsController,
   getContactByIdController,
+  patchContactController,
+  upsertContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
@@ -11,5 +15,13 @@ const router = Router();
 router.get('/', ctrlWrapper(getAllContactsController));
 
 router.get('/:contactId', ctrlWrapper(getContactByIdController));
+
+router.post('/', ctrlWrapper(createContactController));
+
+router.delete('/:contactId', ctrlWrapper(deleteContactController));
+
+router.put('/:contactId', ctrlWrapper(upsertContactController));
+
+router.patch('/:contactId', ctrlWrapper(patchContactController));
 
 export default router;
