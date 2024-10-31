3e5a51fbe6b0503c1f4efe5b56c23c46933d4008
Yaroslav Klimenko <klimyarik13@gmail.com>
2024-10-31 21:15:53 +0200

    123

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -6,9 +6,11 @@ import {
   getContactById,
   updateContact,
 } from '../services/contacts.js';
+import { parsePaginationParams } from '../utils/parsePaginationParams.js';
 
 export const getAllContactsController = async (req, res, next) => {
-  const contacts = await getAllContacts();
+  const { page, perPage } = parsePaginationParams(req.query);
+  const contacts = await getAllContacts({ page, perPage });
   res.status(200).json({
     status: 200,
     message: 'Successfully found contacts!',
---
+++ src/utils/calculatePaginationData.js
@@ -0,0 +1,3 @@
+export const calculatePaginationData = (count, perPage, page) => {
+  const totalPages = Math.ceil(count / perPage);
+};
