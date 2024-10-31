488c150af34312ba8b0c6c1d98a62d85abbad1f3
Yaroslav Klimenko <klimyarik13@gmail.com>
2024-10-31 15:44:50 +0200

    234

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -9,19 +9,39 @@ import {
   upsertContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import {
+  createContactSchema,
+  updateContactSchema,
+} from '../validation/contacts.js';
+import { isValidId } from '../middlewares/isValidId.js';
 
 const router = Router();
 
 router.get('/', ctrlWrapper(getAllContactsController));
 
-router.get('/:contactId', ctrlWrapper(getContactByIdController));
+router.get('/:contactId', isValidId, ctrlWrapper(getContactByIdController));
 
-router.post('/', ctrlWrapper(createContactController));
+router.post(
+  '/',
+  validateBody(createContactSchema),
+  ctrlWrapper(createContactController),
+);
 
-router.delete('/:contactId', ctrlWrapper(deleteContactController));
+router.delete('/:contactId', isValidId, ctrlWrapper(deleteContactController));
 
-router.put('/:contactId', ctrlWrapper(upsertContactController));
+router.put(
+  '/:contactId',
+  isValidId,
+  validateBody(updateContactSchema),
+  ctrlWrapper(upsertContactController),
+);
 
-router.patch('/:contactId', ctrlWrapper(patchContactController));
+router.patch(
+  '/:contactId',
+  isValidId,
+  validateBody(updateContactSchema),
+  ctrlWrapper(patchContactController),
+);
 
 export default router;
---
+++ src/validation/contacts.js
@@ -0,0 +1,63 @@
+import Joi from 'joi';
+
+export const createContactSchema = Joi.object({
+  name: Joi.string().min(3).max(30).required().messages({
+    'string.base': 'Имя должно быть строкой',
+    'string.min': 'Имя должно содержать не менее {#limit} символов',
+    'string.max': 'Имя должно содержать не более {#limit} символов',
+    'any.required': 'Имя обязательно для заполнения',
+  }),
+  phoneNumber: Joi.number().integer().min(6).max(16).required().messages({
+    'number.base': 'Номер телефона должен быть числом',
+    'number.integer': 'Номер телефона должен быть целым числом',
+    'number.min': 'Номер телефона должен содержать не менее {#limit} цифр',
+    'number.max': 'Номер телефона должен содержать не более {#limit} цифр',
+    'any.required': 'Номер телефона обязателен для заполнения',
+  }),
+  email: Joi.string().email().required().messages({
+    'string.base': 'Email должен быть строкой',
+    'string.email': 'Email должен быть действительным',
+    'any.required': 'Email обязателен для заполнения',
+  }),
+  isFavourite: Joi.boolean().messages({
+    'boolean.base': 'Поле "Избранный" должно быть логическим значением',
+  }),
+  contactType: Joi.string()
+    .valid('work', 'home', 'personal')
+    .required()
+    .messages({
+      'string.base': 'Тип контакта должен быть строкой',
+      'any.only':
+        'Тип контакта должен быть одним из следующих: work, home, personal',
+      'any.required': 'Тип контакта обязателен для заполнения',
+    }),
+});
+
+export const updateContactSchema = Joi.object({
+  name: Joi.string().min(3).max(30).messages({
+    'string.base': 'Имя должно быть строкой',
+    'string.min': 'Имя должно содержать не менее {#limit} символов',
+    'string.max': 'Имя должно содержать не более {#limit} символов',
+  }),
+  phoneNumber: Joi.number().integer().min(6).max(16).messages({
+    'number.base': 'Номер телефона должен быть числом',
+    'number.integer': 'Номер телефона должен быть целым числом',
+    'number.min': 'Номер телефона должен содержать не менее {#limit} цифр',
+    'number.max': 'Номер телефона должен содержать не более {#limit} цифр',
+  }),
+  email: Joi.string().email().messages({
+    'string.base': 'Email должен быть строкой',
+    'string.email': 'Email должен быть действительным',
+  }),
+  isFavourite: Joi.boolean().messages({
+    'boolean.base': 'Поле "Избранный" должно быть логическим значением',
+  }),
+  contactType: Joi.string()
+    .valid('work', 'home', 'personal')
+
+    .messages({
+      'string.base': 'Тип контакта должен быть строкой',
+      'any.only':
+        'Тип контакта должен быть одним из следующих: work, home, personal',
+    }),
+});
