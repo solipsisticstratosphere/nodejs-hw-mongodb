5d6f2b71a1988e1e23ca98d60e6254e57ec0e34f
Yaroslav Klimenko <klimyarik13@gmail.com>
2024-11-25 15:30:35 +0200

    123

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -1,18 +1,25 @@
-import cloudindary from 'cloudinary';
+import { v2 as cloudinary } from 'cloudinary';
 import fs from 'node:fs/promises';
 import { env } from './env.js';
 
-import { CLOUDINARY } from '../constants/index.js';
-
-cloudindary.v2.config({
-  secure: true,
-  cloud_name: env(CLOUDINARY.CLOUD_NAME),
-  api_key: env(CLOUDINARY.API_KEY),
-  api_secret: env(CLOUDINARY.API_SECRET),
+const cloud_name = env('CLOUDINARY_CLOUD_NAME');
+const api_key = env('CLOUDINARY_API_KEY');
+const api_secret = env('CLOUDINARY_API_SECRET');
+cloudinary.config({
+  cloud_name,
+  api_key,
+  api_secret,
 });
 
-export const saveFileToCloudinary = async (file) => {
-  const response = await cloudindary.v2.uploader.upload(file.path);
-  await fs.unlink(file.path);
-  return response.secure_url;
+export const saveFileToCloudinary = async (file, folder) => {
+  try {
+    const response = await cloudinary.uploader.upload(file.path, {
+      folder,
+    });
+    return response.secure_url;
+  } catch (error) {
+    throw error;
+  } finally {
+    await fs.unlink(file.path);
+  }
 };
